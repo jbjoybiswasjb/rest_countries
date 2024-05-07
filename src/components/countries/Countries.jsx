5440757385f1222049cf1d0deedf819fbaeb72ac
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-05-07 10:49:02 +0600

    Call the api using useEffect.

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Countries = () => {
 
     const [countries, seCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(countries => console.log(countries))
+    }, []);
 
     return (
         <div>
