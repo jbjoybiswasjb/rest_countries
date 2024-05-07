0908e9b5926558de25cb9137018d1cd787b7f832
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-05-07 10:32:26 +0600

    Just added Countries component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Countries from './components/Countries'
 
 function App() {
   return (
     <>
       <h1>React Rest Countries</h1>
+
+      <Countries></Countries>
     </>
   )
 }
---
+++ src/components/Countries.jsx
@@ -0,0 +1,12 @@
+const Countries = () => {
+
+    
+
+    return (
+        <div>
+            <h2>Countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
