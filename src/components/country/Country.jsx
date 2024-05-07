364040f6aa3dda1e09a09a1d297c8ab88dca98b8
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-05-07 16:37:42 +0600

    removed text align center and addedarea, population dynamically.

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,12 @@
 import './Country.css'
 const Country = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, population, area} = country;
     return (
         <div className='country_div'>
             <h4>Name: {name?.common}</h4>
             <img src={flags?.png} alt={name?.common} />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
         </div>
     );
 };
